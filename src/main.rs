65bcff0a6c23d3292204cbe946b34192ed72c7b6
Rod Hinman <rod@auroraresearch.com>
2021-01-17 21:40:07 -1000

    Discovery book Chapter 8, using peripheral access crate
    Created an initialization function that sets up the port for LEDs and then returns a GPIOE struct that can be used to turn on LEDs.
    Uses the f3::hal::stm32f30x peripheral crate, not
    the f3::hal layer, or
    the stm32f3xx_hal crate created by someone else. (want to try those next).

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,67 @@ use cortex_m_rt::entry;
 use cortex_m::{iprintln, Peripherals};
 //use cortex_m_semihosting::{hprintln};
 
+use f3::hal::stm32f30x::{self, gpioc, rcc, GPIOE, RCC};
+
+fn init() -> GPIOE {
+    let dp = stm32f30x::Peripherals::take().unwrap(); // Device peripherals
+
+    // Use the Reset Clock Control peripheral to enable port E (bit 21 on AHBENR).
+    dp.RCC.ahbenr.modify(|_, w| {
+        w.iopeen().set_bit()
+    });
+
+    // Make thees port E pins outputs.
+    dp.GPIOE.moder.modify(|_, w| {
+        w.moder8().output();
+        w.moder9().output()
+    });
+
+    // Return an owned GPIOE struct.  I think this also means that the rest of the device peripherals are inaccessible.
+    dp.GPIOE
+}
+
 #[entry]
 fn main() -> ! {
     // Set up ITM monitoring support.  To print to the console, use the iprintln!(stim, "...") or iprint!(stim, "...") macros.
-    let mut p = Peripherals::take().unwrap();
+    // See the "itm.rs" example.
+    let mut p = Peripherals::take().unwrap();  // Cortex core peripherals
     let stim = &mut p.ITM.stim[0];
 
+    // This approach works, where the device peripheral is owned by main().
+    // Commented out to try to write an initialization function.
+    // let dp = stm32f30x::Peripherals::take().unwrap(); // Device peripherals
+    // dp.RCC.ahbenr.modify(|_, w| {w.iopeen().set_bit()}); // Turn on bus clock for GPIOE peripheral
+    // let gpioe = dp.GPIOE;
+
+    // rcc.ahbenr.modify(|_, w| {
+    //     w.iopeen().set_bit()
+    // });
+
+    // // Make pins outputs
+    // gpioe.moder.modify(|_, w| {
+    //     w.moder8().output();
+    //     w.moder9().output()
+    // });
+
+    // Alternate approach with initialization.
+    let gpioe = init(); // Returns an owned GPIOE.
+
+
+    gpioe.odr.write(|w| {
+        w.odr8().set_bit();
+        w.odr9().set_bit()
+    });
+
+//    let rcc = unsafe {&*RCC::ptr()};
+//    rcc.ahbenr.modify(|_, w| {w.iopeen().set_bit()});
+    // let gpioe = unsafe { &*GPIOE::ptr()};
+
     iprintln!(stim, "Hello, big world!");
+    // unsafe {
+    //     const GPIO_BSRR: u32 = 0x48001018;
+    //     *(GPIO_BSRR as *mut u32) = 1 << 25;
+    // }
 //    hprintln!("Hello, whacky world!").unwrap(); // This if we are using semihosting.
 
     let mut x = 0;
