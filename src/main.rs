78f20da7da8178a2c80993c247011eaef48e05fb
Rod Hinman <rod@auroraresearch.com>
2021-02-20 11:50:25 -1000

    PWM and Countdown timer using stm32f3xx-hal
    Clean out f3 crate and most direct use of peripheral access crate.
    Implement delay 3 ways:
    * cortex delay
    * HAL Delay struct that uses the SYSTICK
    * HAL Timer struct that uses timer 3, and has the possibility of being blocking or non-blocking.
    
    Also set up PWM to flash LEDs without further code action.

--- src/main.rs
+++ src/main.rs
@@ -9,56 +9,29 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use stm32f3xx_hal as hal;
-
 #[allow(unused_imports)]
 use cortex_m_rt::entry;
 use cortex_m::{iprintln, Peripherals};
 //use cortex_m_semihosting::{hprintln};
 
+use nb::block;  // Needed for the block! macro.
+
+use stm32f3xx_hal as hal;
+
 use hal::pac;
 use hal::prelude::*;
+use hal::pwm::tim1;
+use hal::timer::Timer;
+use hal::delay::Delay;
+//use hal::timer::Timer::tim3;
 
-//use f3::hal::stm32f30x::{self, gpioc, rcc, GPIOE, RCC, TIM6, tim6};
+// The following imports are in the PWM example, but are not necessary.
+//use hal::hal::PwmPin;
+//use hal::flash::FlashExt;
+//use hal::gpio::GpioExt;
+//use hal::rcc::RccExt;
+//use hal::time::U32Ext;
 
-// fn init() -> (GPIOE, TIM6) {
-//     let dp = stm32f30x::Peripherals::take().unwrap(); // Device peripherals
-
-//     // Use the Reset Clock Control peripheral to enable port E (bit 21 on AHBENR).
-//     dp.RCC.ahbenr.modify(|_, w| {
-//         w.iopeen().set_bit()
-//     });
-
-//     // Make thees port E pins outputs.
-//     dp.GPIOE.moder.modify(|_, w| {
-//         w.moder8().output();
-//         w.moder9().output()
-//     });
-
-//     // Use the RCC peripheral to enable timer 6.
-//     dp.RCC.apb1enr.modify(|_, w| {
-//         w.tim6en().set_bit()
-//     });
-
-//     // Set up common parameters for timer 6
-//     // 8 MHz / (psc+1) = 1 kHz is the goal, thus 7999.
-//     dp.TIM6.psc.write(|w| {
-//         w.psc().bits(7999)
-//     });
-
-//     // OPM = one pulse mode, CEN = counter enable, and we want to keep it disabled for now.
-//     dp.TIM6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
-
-//     // Return an owned GPIOE struct.  I think this also means that the rest of the device peripherals are inaccessible.
-//     (dp.GPIOE, dp.TIM6)
-// }
-
-// fn my_delay(timeinfo: &TIM6, ms: u16) {
-//     timeinfo.arr.write(|w| w.arr().bits(ms)); // Set the auto reload value.
-//     timeinfo.cr1.modify(|_, w| w.cen().set_bit()); // Enable timer.
-//     while !timeinfo.sr.read().uif().bit_is_set() {} // Wait until update
-//     timeinfo.sr.modify(|_, w| w.uif().clear_bit());  // Clear the flag for next time.
-// }
 
 #[entry]
 fn main() -> ! {
@@ -67,20 +40,71 @@ fn main() -> ! {
     let mut p = Peripherals::take().unwrap();  // Cortex core peripherals
     let stim = &mut p.ITM.stim[0];
 
+    // Get peripherals
     let dp = pac::Peripherals::take().unwrap(); // Device peripherals
-
+    let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr); // Configure clocks and freeze them.  Need 8 MHz for ITM?.
 
+    // Delay struct using the SYSTICK that I can use for blocking delays.
+    let mut mydelay = Delay::new(p.SYST, clocks);
+
+    // Configure pins
+    // We aren't using the port A or B pins in this example.
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+//    let pa4 = gpioa.pa4.into_af2(&mut gpioa.moder, &mut gpioa.afrl);
+    let _pa6 = gpioa.pa6.into_af2(&mut gpioa.moder, &mut gpioa.afrl);
+//    let pa7 = gpioa.pa7.into_af2(&mut gpioa.moder, &mut gpioa.afrl);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let _pb0 = gpiob.pb0.into_af2(&mut gpiob.moder, &mut gpiob.afrl);
+//    let pb1 = gpiob.pb1.into_af2(&mut gpiob.moder, &mut gpiob.afrl);
+    let _pb4 = gpiob.pb4.into_af2(&mut gpiob.moder, &mut gpiob.afrl);
+
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    // output for TIM1
+    // PE9 is the red "North" LED.
+    // PE11 is the green "East" LED.
+ //   let pe8 = gpioe.pe8.into_af2(&mut gpioe.moder, &mut gpioe.afrh);
+    let pe9 = gpioe.pe9.into_af2(&mut gpioe.moder, &mut gpioe.afrh);
+    let pe11 = gpioe.pe11.into_af2(&mut gpioe.moder, &mut gpioe.afrh);
+    // regular push-pull output PE13 is the red "South" LED.
     let mut led = gpioe.pe13.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
     led.set_low().unwrap();
 
+    // Configure TIM3, one of the general-purpose timers.
+    // This is part of the Timer struct and timer module.
+    // It can be used for blocking/nonblocking delays via the nb crate.
+    let mut mytim3 = Timer::tim3(dp.TIM3, 1000.hz(), clocks, &mut rcc.apb1);
+
+
+    // Configure TIM1, an advanced timer with complementary pins that drive 
+    // the LEDs on the discovery board.  Unfortunately, presently one can use only regular
+    // or complementary pins with the HAL, not both at the same time.
+    // Flash the LEDs autonomously.
+    // From the hal pwm module.
+    let tim1_channels = tim1(
+        dp.TIM1,
+        1280,    // resolution
+        1.hz(),  // Frequency
+        &clocks, // To get clock frequencies
+    );
+
+    let mut tim1_ch1 = tim1_channels.0.output_to_pe9(pe9); // Can't stack .output_to_pe8(pe8) on it, due to the complementary issue.
+    tim1_ch1.set_duty(tim1_ch1.get_max_duty() / 2); // 50% duty
+    tim1_ch1.enable();
+    let mut tim1_ch2 = tim1_channels.1.output_to_pe11(pe11);
+    tim1_ch2.set_duty(tim1_ch2.get_max_duty() / 5); // 20% duty
+    tim1_ch2.enable();
+
     iprintln!(stim, "Hello, big world!");
 
+    // Loop, flashing the South LED manually.
     loop {
         led.toggle().unwrap();
-        cortex_m::asm::delay(8_000_000);
+//        cortex_m::asm::delay(8_000_000);
+        mydelay.delay_ms(1000u16);
         // Toggle by hand
         // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
         if led.is_set_low().unwrap() {
@@ -88,64 +112,11 @@ fn main() -> ! {
         } else {
             led.set_low().unwrap();
         }
-        cortex_m::asm::delay(8_000_000);
+        // Three different ways of delaying.
+//        cortex_m::asm::delay(8_000_000);
+//        mydelay.delay_ms(1000u16);
+        mytim3.start(10.hz()); // 0.1 second delay.  The weird thing is that https://docs.rs/stm32f3xx-hal/0.6.1/stm32f3xx_hal/prelude/trait._embedded_hal_timer_CountDown.html
+                               // says it wants a time, but instead it wants a Hertz struct.
+        block!(mytim3.wait()).unwrap();  // Block until the timer times out.
     }
-
-    // This approach works, where the device peripheral is owned by main().
-    // Commented out to try to write an initialization function.
-    // let dp = stm32f30x::Peripherals::take().unwrap(); // Device peripherals
-    // dp.RCC.ahbenr.modify(|_, w| {w.iopeen().set_bit()}); // Turn on bus clock for GPIOE peripheral
-    // let gpioe = dp.GPIOE;
-
-    // rcc.ahbenr.modify(|_, w| {
-    //     w.iopeen().set_bit()
-    // });
-
-    // // Make pins outputs
-    // gpioe.moder.modify(|_, w| {
-    //     w.moder8().output();
-    //     w.moder9().output()
-    // });
-
-    // Alternate approach with initialization.
-    // let (gpioe, mytim6) = init(); // Returns an owned GPIOE.
-
-
-    // gpioe.odr.write(|w| {
-    //     w.odr8().set_bit();
-    //     w.odr9().set_bit()
-    // });
-
-    // Third approach using the HAL crate.  Taken from the f3 repo examples/blinky.rs.
-    // But it doesn't compile, as the various FLASH, RCC, GPIOE structs are from the peripheral access crate and don't have
-    // constrain or split methods.
-    // let dp = stm32f30x::Peripherals::take().unwrap(); // Device peripherals
-    // let mut flash = dp.FLASH.constrain();
-    // let mut rcc = dp.RCC.constrain();
-    // let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-
-
-//    let mut x = 0;
-    // let ms = 500;
-    // loop {
-    //     // your code goes here
-    //     // x += 1;
-    //     // iprintln!(stim, "x is {}", x);
-        
-    //     gpioe.odr.write(|w| {
-    //         w.odr8().set_bit();
-    //         w.odr9().set_bit()
-    //     });
-
-    //     my_delay(&mytim6, ms);
-
-    //     gpioe.odr.write(|w| {
-    //         w.odr8().clear_bit();
-    //         w.odr9().clear_bit()
-    //     });
-
-    //     my_delay(&mytim6, ms);
-
-    // }
 }
